ecd02961988d9d07ac9c8946b5548a09aaf11407
SeiichiN <billie175@gmail.com>
2019-02-19 08:14:37 +0900

    max_ascii関数 とりあえずできた。

--- ex3-11-4.ml
+++ ex3-11-4.ml
@@ -5,3 +5,46 @@ String.length "Hello!";;
 
 int_of_char 'A';;
 
+  (* ふたつのchar型の引数をとり、ASCIIコードの大きいほうを返す関数 *)
+let takeBig (a, b) =
+  if (int_of_char a) >= (int_of_char b)
+  then a
+  else b;;
+
+  (* TEST *)
+let test1 = takeBig('a', 'b') = 'b';;
+let test2 = takeBig('A', 'a') = 'a';;
+
+let moji = "Hello!";;
+let i = 0;;
+let (a, b) = (moji.[i], moji.[i+1]);;
+
+let test3 = takeBig(a, b) = b;;
+
+let rec hikaku (moji, i, a) =
+  if i = String.length moji
+  then a
+  else
+    let a = takeBig (a, moji.[i]) in
+    hikaku (moji, i+1, a)
+
+(* TEST *)
+let test11 = hikaku ("abcde", 1, 'a') = 'e';;
+  
+  
+let max_ascii moji =
+  let rec hikaku (moji, i, a) =
+    if i = String.length moji                          
+    then a
+    else
+      let a = takeBig (a, moji.[i]) in
+      hikaku(moji, i+1, a)
+  in
+  hikaku (moji, 1, moji.[0])
+                  
+  (* TEST *)
+let test4 = max_ascii "abcde" = 'e';;
+let test5 = max_ascii "Az" = 'z';;
+  
+
+
