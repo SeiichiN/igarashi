9634a3533d6a7177f95e39604e61197c98ff933a
SeiichiN <billie175@gmail.com>
2019-03-09 09:58:30 +0900

    take drop dekita.

--- ex5-2-8.ml
+++ ex5-2-8.ml
@@ -1,5 +1,7 @@
 (* 練習問題 5.2.8 *)
 
+#use "ex5-2.ml";; (* 関数 downto1 *)
+
 (*
  * 先頭からn番目までの要素からなる部分リストを取り出す関数 take と
  * n番目までの要素を抜かした部分リストを取り出す関数 drop
@@ -13,16 +15,26 @@ let take n l =
             if n = 0
             then a
             else
-                v :: take_inner (n-1) rest :: a
+                v :: take_inner (n-1) rest a
     in
     take_inner n l [];;
 
 
-#use "ex5-2.ml";; (* 関数 downto1 *)
-
 let ten_to_zero = downto1 10;;
 
 let test1 = take 8 ten_to_zero = [10; 9; 8; 7; 6; 5; 4; 3];;
+
+
+let rec drop n l =
+    match l with
+    [] -> []
+    | v :: rest ->
+            if n = 0
+            then
+                l
+            else
+                drop (n-1) rest;;
+
 
 let test2 = drop 7 ten_to_zero = [3; 2; 1];;
 
