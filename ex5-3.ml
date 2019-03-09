323a7ee8c17eb81fd76dac77022e95e20dae07b7
SeiichiN <billie175@gmail.com>
2019-03-09 18:13:45 +0900

    ex5-3.ml

--- ex5-3.ml
+++ ex5-3.ml
@@ -24,27 +24,67 @@ let cons a b = a :: b;;
 let test99 = cons 1 [] = [1];;
 (* let test98 = cons 1 2 = [1; 2];; *)
 
+(* ========================================================= *)
 (* 2: intersect s1 s2 で s1 と s2 の共通部分を返す関数 *)
 
+let test97 = mem 3 [1; 2; 3; 4; 5; 6] = true;;
+let test96 = mem 7 [1; 2; 3; 4; 5; 6] = false;;
+
+(* 補助関数: リスト l の末尾に a を加える関数 *)
+let rec add_last a l =
+    match l with
+    [] -> a :: []
+    | v :: rest ->
+            v :: (add_last a rest);;
+
+let test95 = add_last 3 [2; 4; 6] = [2; 4; 6; 3];;
+
+(* 2つのリスト s1 と s2 の共通部分を返す関数 *)
 let intersect s1 s2 =
     let rec intersect_in s1 s2 common =
         match s1 with
         [] -> common
-    | v1 :: rest1 ->
-            match s2 with
-            [] -> []
-    | v2 :: rest2 ->
-            if v1 = v2
-            then
-                intersect_in v1 rest2 (v1 :: common)
-            else
-    intersect_in v1 rest2 common
-
+    | v :: rest ->
+            if (mem v s2)
+            then intersect_in rest s2 (add_last v common)
+            else intersect_in rest s2 common
+    in
+    intersect_in s1 s2 [];;
 
 let test11 = intersect s1 s2 = [2; -5; 4];;
 
-
+(* ================================================================ *)
 (* 3: union s1 s2 で s1 と s2 の和を返す関数 union *)
 
+let union s1 s2 =
+    let rec union_in s1 s2 ac =
+        match s1 with
+        [] -> ac @ s2
+    | v :: rest ->
+            if (mem v s2)
+            then union_in rest s2 ac
+            else union_in rest s2 (add_last v ac)
+    in
+    union_in s1 s2 [];;
 
+let test21 = union s1 s2 = [1; 20; 2; 3; -5; 34; 4];;
+
+(* ================================================================ *)
 (* 4: diff s1 s2 で s1 と s2 の差を返す関数 diff *)
+
+(* 和集合から共通集合を引くことで、差の集合がでる *)
+let diff s1 s2 =
+    let inter_list = intersect s1 s2 in (* 共通部分 *)
+    let union_list = union s1 s2 in (* 和 *)
+    let rec diff_in un_list in_list ac =
+        match un_list with
+        [] -> ac
+    | v :: rest ->
+            if (mem v in_list)
+            then diff_in rest in_list ac
+            else diff_in rest in_list (add_last v ac)
+    in
+    diff_in union_list inter_list [];;
+
+let test31 = diff s1 s2 = [1; 20; 3; 34];;
+
