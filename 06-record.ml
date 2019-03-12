5dfd6529eea9779003c56688a3aba089a30c2367
Seiichi Nukayama <billie175@gmail.com>
2019-03-12 17:37:50 +0900

    06-record.ml modified.

--- 06-record.ml
+++ 06-record.ml
@@ -51,3 +51,38 @@ type figure =
     | Rectangle of int * int
     | Square of int;;
 
+let c = Circle 3;;
+
+let figs = [Point; Square 5; Rectangle (4, 5); c];;
+
+
+let area_of_figure = function
+    Point -> 0
+    | Circle r -> r * r * 3
+    | Rectangle (x, y) -> x * y
+    | Square x -> x * x;;
+
+
+let map f l =
+    let rec map_in f l e =
+        match l with 
+    [] -> []
+    | v :: rest ->
+            (f v) :: map_in f rest e
+    in
+    map_in f l [];;
+
+area_of_figure c;;
+
+map area_of_figure figs;;
+
+let similar x y =
+    match (x, y) with
+    (Point, Point) | (Circle _, Circle _) | (Square _, Square _) ->true
+    | (Rectangle (l1, l2), Rectangle (l3, l4)) ->
+            (l3 * l2 - l4 * l1) = 0
+    | _ -> false;;
+
+similar (Rectangle (2, 4)) (Rectangle (1, 2));;
+
+
