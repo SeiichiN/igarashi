132e19ad62c1e7402dc8955b83ccf82de8a55eb4
SeiichiN <billie175@gmail.com>
2019-03-16 21:54:12 +0900

    ex6-2.ml modified.

--- ex6-2.ml
+++ ex6-2.ml
@@ -24,12 +24,42 @@ type 'a with_location = { loc_x: float; loc_y: float; body: 'a};;
 let rec1 = { loc_x = 4.; loc_y = 2.; body = Rectangle (4, 2) };;
 let rec2 = { loc_x = 6.; loc_y = 3.; body = Rectangle (5, 3) };;
 
+(* 自乗 *)
+let joh (x: float) = x *. x;;
+let test1 = joh 2. = 4.;;
+let test2 = joh 3. = 9.;;
+
+let abs (x: float) =
+    if x < 0. then -1. *. x else x;;
+
 let overlap a b =
     match (a.body, b.body) with
     Point, Point -> true
     | Circle c1, Circle c2 ->
-            ((a.loc_x -. b.loc_x) *. (a.loc_x -. b.loc_x) +.
-            (a.loc_y -. b.loc_y) *. (a.loc_y -. b.loc_y) ) >
-            (c1 + c2) * (c1 + c2);;
+            let d1 = float_of_int c1 in
+            let d2 = float_of_int c2 in
+            if (joh(a.loc_x -. b.loc_x) +. joh(a.loc_y -. b.loc_y))
+            < joh(d1 +. d2)
+            then true
+            else false
+    | Rectangle (l1, l2), Rectangle (l3, l4) ->
+            let d1 = float_of_int l1 in
+            let d2 = float_of_int l2 in
+            let d3 = float_of_int l3 in
+            let d4 = float_of_int l4 in
+            if (abs(a.loc_x -. b.loc_x) < (d1 +. d3) /. 2.) &&
+            (abs(a.loc_y -. b.loc_y) < (d2 +. d4) /. 2.) 
+            then true
+            else false;;
+
+let cir1 = { loc_x = 3.; loc_y = 3.; body = Circle 2 };;
+let cir2 = { loc_x = 1.; loc_y = 1.; body = Circle 3 };;
+
+let test3 = overlap cir1 cir2 = true;;
+
+let cir3 = {loc_x = 5.; loc_y = 5.; body = Circle 1};;
+let cir4 = {loc_x = 1.; loc_y = 1.; body = Circle 2};;
+
+let test4 = overlap cir3 cir4 = false;;
 
 
