8dd0f20911a7bd176f9ee20909b5463abe8016f0
SeiichiN <billie175@gmail.com>
2019-03-16 10:18:28 +0900

    ex6-1.ml modified.

--- ex6-1.ml
+++ ex6-1.ml
@@ -7,20 +7,21 @@ type figure =
     Point
     | Circle of int
     | Rectangle of int * int
-    | Square of int * int;;
+    | Square of int;;
 
 let similar x y =
     match (x, y) with
     (Point, Point)
-    | (Circle _, Circle _) -> true
-    | (Square (l1, l2), Square (l3, l4)) -> 
-            (l1 - l2) = 0 && (l3 - l4) = 0
+    | (Circle _, Circle _)
+    | (Square _, Square _) -> true
     | (Rectangle (l1, l2), Rectangle (l3, l4)) ->
             (l3 * l2 - l4 * l1) = 0
+    | (Rectangle (l1, l2), Square _) ->
+            (l1 - l2) = 0
     | _ -> false;;
 
 similar (Rectangle (2, 4)) (Rectangle (1, 2));;
 
 similar (Circle 3) (Circle 5);;
 
-            
+similar (Rectangle (3, 3)) (Square 6);;            
