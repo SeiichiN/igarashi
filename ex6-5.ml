fff3653cf820b8793c20e0c9c96a1034ab2bec5f
Seiichi Nukayama <billie175@gmail.com>
2019-03-19 12:14:51 +0900

    ex6-5 modified.

--- ex6-5.ml
+++ ex6-5.ml
@@ -16,11 +16,18 @@ comptree の例
  Br(x, Br(x, Br(x, Lf, Lf), Br(x, Lf, Lf)),
                   Br(x, Br(x, Lf, Lf), Br(x, Lf, Lf)));;
  *)
+let joh x n =
+    let rec joh_in x n a =
+        if n = 0 then a
+    else joh_in x (n-1) (x * a)
+    in
+    joh_in x n 1;;
+
           
 let comptree' n =
   let rec comptree_in x n =
     if n = 0 then Lf
-    else Br ((n-1), (comptree_in (2 * (n-1) + 1) (n-1)), (comptree_in (2 * (n-1)) (n-1)))
+    else Br ((joh 2 (n-1)), (comptree_in ((joh 2 n) + 1) (n-1)), (comptree_in (joh 2 n) (n-1)))
   in 
   comptree_in 1 n;;
   
