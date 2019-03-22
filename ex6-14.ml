f48a0e5241bb2d38f55c6a5f585c5eb928cf1ca2
SeiichiN <billie175@gmail.com>
2019-03-22 17:44:05 +0900

    ex6-14.ml modified.

--- ex6-14.ml
+++ ex6-14.ml
@@ -17,3 +17,55 @@ let rec prime_seq primes x = ...
 - : int = 71
 
  *)
+
+
+type intseq = Cons of int * (int -> intseq);;
+
+
+(* n番目の要素を取り出す関数 *)
+let rec nthseq n (Cons(x, f)) =
+    if n = 1 then x
+    else nthseq (n-1) (f x);;
+
+(* 素数かどうかを調べる関数 *)
+let is_prime x =
+    let rec is_divisible_from_2_to n =
+        (n > 1) && ((x mod n = 0) || is_divisible_from_2_to (n-1))
+    in
+    not (is_divisible_from_2_to (x-1));;
+
+let test1 = is_prime 13 = true;;
+let test2 = is_prime 14 = false;;
+
+(* 素数かどうかを調べる。小さい数から順に *)
+let is_prime1 x =
+    let rec is_divisible_from_2_to n =
+        if n = x then false
+        else
+            (n > 1) && ((x mod n = 0) || is_divisible_from_2_to (n+1))
+    in
+    not (is_divisible_from_2_to 2);;
+
+(* 素数かどうかを調べる。割る数の最大は、悪数の平方根 *)
+let is_prime2 x =
+    let rec is_divisible_from_2_to n =
+        if n = int_of_float(floor(sqrt (float_of_int x))) then false
+        else
+            (n > 1) && ((x mod n = 0) || is_divisible_from_2_to (n+1))
+    in
+    not (is_divisible_from_2_to 2);;
+
+
+
+(* 与えられた x より大きい最小の素数を返す関数 *)
+let rec next_prime x =
+    if is_prime (x+1) then x+1
+    else next_prime (x+1);;
+
+(* 素数の無限列 *)
+let rec prime_seq x =
+    if is_prime2 (x+1)
+    then Cons(x+1, prime_seq)
+    else prime_seq (x+1);;
+
+let test_nth_prime = nthseq 20 (prime_seq 1) = 71;;
