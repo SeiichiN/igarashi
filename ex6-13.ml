10061b69daa30548026a70e159a1da97948c187f
SeiichiN <billie175@gmail.com>
2019-03-22 12:37:43 +0900

    ex6-13.ml 難しかった。

--- ex6-13.ml
+++ ex6-13.ml
@@ -12,24 +12,30 @@ step 関数をヒントにして、フィボナッチ数を表す無限列を定
 
 type intseq = Cons of int * (int -> intseq);;
 
-(* step 関数 *)
-
-let rec step n x = Cons (x+n, step (n+1));;
-  
-let Cons(x1, f1) = step 1 0
-let Cons(x2, f2) = f1 x1;;
-
-  (* n番目の要素を取り出す関数 *)
-let rec nthseq n (Cons(x,f)) =
-  if n = 1 then x
-  else nthseq (n-1) (f x);;
-
 let rec fibo x =
   if x < 3 then 1
   else
     fibo (x-1) + fibo (x-2);;
   
 let rec fib x =
-  if x < 3 then Cons (1, fib)
-  else
-    Cons (fibo(x-1) + fibo(x-2), fib);;
+    if x < 3 then Cons (1, fib)
+    else
+      Cons (fibo(x-1) + fibo(x-2), fib);;
+
+
+let nthseq n f =
+  let nthseq_in n (Cons (x, f)) =
+    if n < 3 then 1
+    else x
+  in
+  nthseq_in n (f n);;
+
+(* 正解 http://gifnksm.hatenablog.jp/entry/20080316/1205700453 *)
+let fib =
+  let rec f x y = Cons (x, f (x+y)) in
+  Cons (1, f 1);;
+
+let rec nthseq n (Cons (x, f)) =
+  if n = 1 then x
+  else nthseq (n-1) (f x);;
+
