2c3219408f894591c18d59e054e51cbebc1198e6
SeiichiN <billie175@gmail.com>
2019-03-25 08:17:43 +0900

    ex7-3.ml modified.

--- ex7-3.ml
+++ ex7-3.ml
@@ -54,5 +54,32 @@ let rec change coins amount =
 
  *)
 
+let rec change coins amount =
+  match (coins, amount) with
+    (_, 0) -> []
+  | ((c :: rest) as coins, total) ->
+     if c > total then change rest total
+     else c :: change coins (total - c);;
 
+let us_coins = [25; 10; 5; 1] (* アメリカのコイン *)
+and gb_coins = [50; 20; 10; 5; 2; 1] (* イギリスのコイン *) ;;
 
+let test1 = change gb_coins 43 = [20; 20; 2; 1];;
+let test2 = change us_coins 43 = [25; 10; 5; 1; 1; 1];;
+  
+  (*
+let rec change coins amount =
+    match (coins, amount) with
+      (_, 0) -> []
+    | ((c :: rest) as coins, total) ->
+        if c > total then change rest total
+        else
+            (try
+                c :: change coins (tatal - c)
+             with Failure "change" -> ... )
+    | _ -> ... ;;
+
+  
+let test11 = change [5; 2] 16 = [5; 5; 2; 2; 2];;
+
+   *)
