3c7ba518b0ccbafe5f8e3e64e36b51a894a8f7f8
SeiichiN <billie175@gmail.com>
2019-04-01 17:36:36 +0900

    08-queue.ml modified.

--- 08-queue.ml
+++ 08-queue.ml
@@ -23,6 +23,18 @@ let add a = function
             q.tail <- c
     | _ -> failwith "enqueue: input queue broken";;
 
-add 1 q; add 2 q; add 3 q;;
+add 1 q;;
 q;;
+
+(* peek -- 先頭要素を返す関数 *)
+let peek = function
+    {head = MNil; tail = MNil} -> failwith "ht: queue is empty"
+  |{head = MCons(a, _)} -> a
+  | _ -> failwith "hd: queue is broken";;
+
+(* take -- 先頭要素を削除して、その先頭要素を返す関数 *)
+let take = function
+    {head = MNil; tail = MNil} ->failwith "dequeue: queue is empty"
+  | {head = MCons(a, next)} as q -> q.head <- !next; a
+  | _ -> failwith "dequeue: queue is broken";;
 
