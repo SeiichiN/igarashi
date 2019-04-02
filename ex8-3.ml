efe5327c9498685b0c0ae75650f3da233ff90737
SeiichiN <billie175@gmail.com>
2019-04-02 18:50:26 +0900

    ex8-3.ml dekita.

--- ex8-3.ml
+++ ex8-3.ml
@@ -18,12 +18,19 @@
    - : int = 120
  *)
 
-let f = ref (fun y -> y + 1)
+let f = ref (fun y -> y + 1);;
+   (* f -- 引数を 1 増加させて返すという関数が書き換え可能な参照として定義されている。
+    # !f 5;; とすると、6 が返ってくる。
+    しかし、この f 関数は、下の funny_fact の定義のあと、別の関数がセットされるので、（funny_factがセットされるので、別に (fun y -> y) という恒等関数でもかまわないはず。
+    * *)
+
 let funny_fact x =
   if x = 1 then 1
   else x * (!f (x -1));;
+   (* !f で、(x-1) を引数として、f にセットされた関数を実行する。 *)
 
 f := funny_fact;;
+   (* f に、関数 funny_fact がセットされている。これにより、funny_fact の中の f! は、funny_fact ということになる。 *)
 
 funny_fact 5;;
 
