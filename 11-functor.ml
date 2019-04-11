ed1eef0e1d05d7cbfcb0b826d2a8534d015465bb
SeiichiN <billie175@gmail.com>
2019-04-11 17:43:19 +0900

    11-functor.ml modified.

--- 11-functor.ml
+++ 11-functor.ml
@@ -296,3 +296,92 @@ let s3 = inter s1 s2 in (mem 1 s3, mem 2 s3, mem 3 s3);;
 (* elements -- 要素をリストとして列挙する *)
 let test_elements1 = elements s1 = [1; 2];;
 let test_elements2 = elements s2 = [1; 3];;
+
+(* シグネチャを分離する *)
+(*
+module type SET with type elt = Order.t =
+       sig
+         type elt
+         type t
+         val empty : t
+         val mem : elt -> t -> bool
+         val add : elt -> t -> t
+         val inter : t -> t -> t
+         val elements : t -> elt list
+       end;;
+*)
+
+module type SET =
+sig
+  type elt 
+  type t
+  val empty : t
+  val mem : elt -> t -> bool
+  val add : elt -> t -> t
+  val inter : t -> t -> t
+  val elements : t -> elt list
+end;;
+(*
+module type SET =
+     sig
+       type elt
+       type t
+       val empty : t
+       val mem : elt -> t -> bool
+       val add : elt -> t -> t
+       val inter : t -> t -> t
+       val elements : t -> elt list
+     end
+ *)
+
+
+module MakeAbstractSet (Order : OrderedType)
+  : SET with type elt = Order.t
+=
+struct
+  type elt = Order.t
+  type t = elt list
+
+  let empty = []
+
+  let rec mem elt = function
+      [] -> false
+    | x :: rest ->
+      let r = Order.compare elt x in
+      (r = 0) || ((r > 0) && mem elt rest)
+  (* if elt = x then true else mem elt rest は、あかんの？*)
+
+  let rec add elt = function
+      [] -> [elt]
+    | (x :: rest as s) ->
+      match Order.compare elt x with
+        0 -> s
+      | r when r < 0 -> elt :: s
+      | _ -> x :: (add elt rest)
+  (* このリストは、昇順にならんでいるリストやな *)
+
+  let rec inter s1 s2 =
+    match (s1, s2) with
+      (s1, []) -> []
+    |([], s2) -> []
+    |((e1::rest1 as s1), (e2::rest2 as s2)) ->
+      match Order.compare e1 e2 with
+        0 -> e1 :: inter rest1 rest2
+      | r when r < 0 -> inter rest1 s2
+      | _ -> inter s1 rest2
+
+  let rec elements s = s
+end;;
+(*
+module MakeAbstractSet :
+     functor (Order : OrderedType) ->
+       sig
+         type elt = Order.t
+         type t
+         val empty : t
+         val mem : elt -> t -> bool
+         val add : elt -> t -> t
+         val inter : t -> t -> t
+         val elements : t -> elt list
+       end
+*)
