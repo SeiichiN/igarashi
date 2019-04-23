0fbe6ca15b6b881dd864436bc9afdd3393c6963a
SeiichiN <billie175@gmail.com>
2019-04-23 12:11:56 +0900

    14-variant.ml modified at BGM 5F

--- 14-variant.ml
+++ 14-variant.ml
@@ -270,3 +270,58 @@ let rec max_list = function
 (* val max_list : [ `Cons of 'a * ([< `Cons of 'a * 'b | `Nil ] as 'b) ] -> 'a =
   <fun> *)
 
+(* ------ 14.3.2 再帰的関数の拡張 ------ *)
+
+(* 拡張リスト構造 *)
+(* `App -- ふたつの `Cons リストを結合する *)
+let l6 = `Cons (1, `App (l2, l3));;
+(* val l6 :
+ *   [> `Cons of
+ *      int *
+ *      [> `App of
+ *         [> `Cons of int * [> `Nil ] ] *
+ *         [> `Cons of int * [> `Cons of int * [> `Nil ] ] ] ] ] =
+ *  `Cons (1, `App (`Cons (1, `Nil), `Cons (2, `Cons (1, `Nil))))  *)
+
+(* 拡張リスト構造の長さを求める *)
+let rec alength = function
+  `Nil -> 0
+  | `Cons (a, l) -> 1 + alength l
+  | `App (l1, l2) -> alength l1 + alength l2;;
+(* val alength : ([< `App of 'a * 'a | `Cons of 'b * 'a | `Nil ] as 'a) -> int =
+ *   <fun>  *)
+
+let rec alength_wrong = function
+  (`Nil | `Cons (_, _)) as l -> length l
+  | `App (l1, l2) -> alength_wrong l1 + alength_wrong l2;;
+(* val alength_wrong :
+ *   ([< `App of 'a * 'a
+ *     | `Cons of 'b * ([ `Cons of 'b * 'c | `Nil ] as 'c)
+ *     | `Nil ]
+ *   as 'a) ->
+ *   int = <fun>  *)
+
+(*  alength_wrong l6;;  *)
+
+let make_length f = function
+  `Nil -> 0
+  | `Cons (a, l) -> 1 + f l;;
+(*  val make_length : ('a -> int) -> [< `Cons of 'b * 'a | `Nil ] -> int = <fun> *)
+
+let rec length l = make_length length l;;
+(* val length : ([< `Cons of 'b * 'a | `Nil ] as 'a) -> int = <fun>  *)
+
+length l3;;  (* - : int = 2  *)
+
+let make_alength f = function
+  (`Nil | `Cons (_, _)) as l -> make_length f l
+  | `App (l1, l2) -> f l1 + f l2;;
+(* val make_alength :
+ *   ('a -> int) -> [< `App of 'a * 'a | `Cons of 'b * 'a | `Nil ] -> int = <fun> *)
+
+let rec alength l = make_alength alength l;;
+(* val alength : ([< `App of 'a * 'a | `Cons of 'b * 'a | `Nil ] as 'a) -> int =
+ *   <fun> *)
+
+alength l6;;  (* - : int = 4 *)
+
