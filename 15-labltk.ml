92820859ff93fd4b660a5145a21fa5327f302241
SeiichiN <billie175@gmail.com>
2019-04-25 21:06:01 +0900

    gui started.

--- 15-labltk.ml
+++ 15-labltk.ml
@@ -1,4 +1,34 @@
 (* 15.1 「銀行口座」を作ってみる *)
 
-label.create;;
-:
+#directory "+labltk";;
+#load "labltk.cma";;
+open Tk;;
+
+Label.create;;
+(*
+- : ?name:string ->
+    ?anchor:anchor ->
+    ?background:color ->
+    ?bitmap:bitmap ->
+    ?borderwidth:int ->
+    ?cursor:cursor ->
+    ?font:string ->
+    ?foreground:color ->
+    ?height:int ->
+    ?highlightbackground:color ->
+    ?highlightcolor:color ->
+    ?highlightthickness:int ->
+    ?image:[< image ] ->
+    ?justify:justification ->
+    ?padx:int ->
+    ?pady:int ->
+    ?relief:relief ->
+    ?takefocus:bool ->
+    ?text:string ->
+    ?textvariable:Textvariable.textVariable ->
+    ?textwidth:int ->
+    ?underline:int ->
+    ?width:int ->
+    ?wraplength:int -> 'a Widget.widget -> Widget.label Widget.widget
+= <fun>
+*)
